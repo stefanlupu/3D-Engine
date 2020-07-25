053013cd1e51d84bddb5f2079b0be3f1144f389c
Stefan N Lupu <stefan.n.lupu@gmail.com>
2020-07-25 18:03:04 +0100

    Test new data-structures and their functions

--- engine.cpp
+++ engine.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include "data-structures.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -7,23 +8,33 @@ const int SCREEN_HEIGHT = 480;
 int main(int argc, char* args[]) {
     // initialize objects
     SDL_Window* window = NULL;
-    SDL_Surface* surface = NULL;
+    SDL_Renderer* renderer = NULL;
 
     // raise an error if SDL fails to load
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
     } else {
         window = SDL_CreateWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        // raise error if window/renderer creation fails
         if (window == NULL) {
-            // raise another error if window creation fails
             printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        } else if (renderer == NULL) {
+            printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
         } else {
-            // simple boiler plate to test if everything is set up correctly 
-            surface = SDL_GetWindowSurface(window);
+            // rendering triangle test
+            triangle triangle = {
+                {{100, 100},{200, 200},{150, 300}}
+            };
 
-            SDL_FillRect(surface, NULL, SDL_MapRGB(surface -> format, 255, 255, 255));
+            // set backgroud to white
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderClear(renderer);
 
-            SDL_UpdateWindowSurface(window);
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            drawTriangle(triangle, renderer);
+
+            SDL_RenderPresent(renderer);
 
             SDL_Delay(2000);
         }
