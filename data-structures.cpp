c186268c9ff4dc4b44b3ea5aefc787b5dc3b44e6
Stefan N Lupu <stefan.n.lupu@gmail.com>
2020-08-21 20:16:42 +0100

    Add perspective matrix and projection function

--- data-structures.cpp
+++ data-structures.cpp
@@ -1,8 +1,29 @@
 #include <SDL2/SDL.h>
+#include <math.h>
 #include "data-structures.h"
 
+// render given triangle onto specified renderer
 void drawTriangle(triangle t, SDL_Renderer* renderer) {
     SDL_RenderDrawLine(renderer, t.verteces[0].x, t.verteces[0].y, t.verteces[1].x, t.verteces[1].y);
     SDL_RenderDrawLine(renderer, t.verteces[1].x, t.verteces[1].y, t.verteces[2].x, t.verteces[2].y);
     SDL_RenderDrawLine(renderer, t.verteces[2].x, t.verteces[2].y, t.verteces[0].x, t.verteces[0].y);
+}
+
+// return a perspective projected version of a given 3D vector
+vector_3D projectVector(vector_3D v, perspectiveProjection m) {
+    vector_3D pv;
+
+    pv.x = v.x * m.cells[0][0] + v.y * m.cells[1][0] + v.z * m.cells[2][0] + m.cells[3][0];
+    pv.y = v.x * m.cells[0][1] + v.y * m.cells[1][1] + v.z * m.cells[2][1] + m.cells[3][1];
+    pv.z = v.x * m.cells[0][2] + v.y * m.cells[1][2] + v.z * m.cells[2][2] + m.cells[3][2];
+
+    float w = v.x * m.cells[0][3] + v.y * m.cells[1][3] + v.z * m.cells[2][3] + m.cells[3][3];
+
+    if (w != 0.0f) {
+        pv.x /= w;
+        pv.y /= w;
+        pv.z /= w;
+    }
+
+    return pv;
 }
--- data-structures.h
+++ data-structures.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <vector>
+#include <math.h>
 
 struct vector_3D {
         float x, y, z;
 };
-
-//vector_3D project(vector_3D v, int);
 
 struct triangle {
         vector_3D verteces[3];
@@ -16,3 +15,18 @@ void drawTriangle(triangle t, SDL_Renderer* renderer);
 struct mesh {
         std::vector<triangle> poligons;
 };
+
+// perspective projection matrix
+const float aspectRatio = 720.0f / 1280.0f;
+const float fovRadians = 1.0f / (tanf(90.0f * 0.5f  * 3.14159f / 180.0f));
+
+struct perspectiveProjection {
+        float cells[4][4] = {
+            { aspectRatio * fovRadians, 0.0f, 0.0f, 0.0f},
+            { 0.0f, fovRadians, 0.0f, 0.0f},
+            { 0.0f, 0.0f, 1000.0f / (1000.0f - 0.1f), 1.0f},
+            { 0.0f, 0.0f, -1000.0f * 0.1 / (1000.0f - 0.1f), 0.0f}
+        };
+};
+
+vector_3D projectVector(vector_3D v, perspectiveProjection m);
